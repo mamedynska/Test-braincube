47bcd1583dd100bf53bd32c75e268de17467c982
Maxime Medynska <m.medynska@outlook.fr>
2020-12-15 18:40:13 +0100

    Adding  comments

--- lineaire.py
+++ lineaire.py
@@ -27,6 +27,9 @@ print(dfResultat)
 print("Coef : ", regSal.coef_)
 print("Intercept : ", regSal.intercept_)
 print("Score : ",regSal.score(X, y))
+# Coef :  [0.67845169]
+# Intercept :  3.487717762891342e-16
+# Score :  0.46029670093656905
 
 # Ecriture dans prediction1.csv
 dfResultat.to_csv("data/prediction_1.csv",index=False)
@@ -42,6 +45,9 @@ print("Score : ",regSalAge.score(X2, y2))
 pred2=regSalAge.predict(X2)
 dfResultat2 = pd.DataFrame({"salaire":X2["salaire"], "age":X2["age"],"depenses":y2, "Depenses Preditent":pred2})
 print(dfResultat2)
+# Coef :  [0.67519274 0.1395618 ]
+# Intercept :  3.724175540664552e-16
+# Score :  0.4797635759377238
 
 # Ecriture dans prediction2_depense.csv
 dfResultat2.to_csv("data/prediction2_depense.csv",index=False)
